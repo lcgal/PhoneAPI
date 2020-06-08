4ec613232a0126d6aa766553b17bdec10bece81f
Lucas Galvão <lcollares@indracompany.com>
2020-06-08 20:52:30 -0300

    migratingToNetCore Adding a method to get information from bgg api

--- PhoneApi/Controllers/CredentialsController.cs
+++ PhoneApi/Controllers/CredentialsController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
+using System.Threading;
 using System.Web;
 using System.Web.Http;
 using System.Xml.Linq;
@@ -129,65 +130,122 @@ namespace PhoneApi.Controllers
         public async System.Threading.Tasks.Task<ApiResponse<Credentials>> TestAsync()
         {
             var response = new ApiResponse<Credentials>();
+
+            List<long> gamesIds = null;
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("App")))
+            {
+                gamesIds = connection.Query<long>($"select * from test").ToList();
+            }
+            
             HttpClient _client = new HttpClient();
             _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/xml"));
 
-            HttpResponseMessage test = _client.GetAsync(new Uri("https://www.boardgamegeek.com/xmlapi/boardgame/161936")).Result;
-
-            EncodingProvider provider = new CustomUtf8EncodingProvider();
-            Encoding.RegisterProvider(provider);
-
-            if (test.StatusCode == HttpStatusCode.OK)
+            string currentRequest = "";
+            int numberOfGames = 0;
+            foreach (long id in gamesIds)
             {
-                XDocument xdoc = XDocument.Parse(test.Content.ReadAsStringAsync().Result);
-
-                StringReader sr = new StringReader(xdoc.ToString());
-
-                XmlSerializer serializer = new XmlSerializer(typeof(boardgames));
-
-                boardgames result = (boardgames)serializer.Deserialize(sr);
-
-                var game = result.boardgame.FirstOrDefault();
-
-                GamesList gamelist = new GamesList();
-                List<GamesMechanic> mechanicslist = new List<GamesMechanic>();
-                List<GamesFamily> familieslist = new List<GamesFamily>();
-
-                gamelist.id = Convert.ToInt64(game.objectid);
-
-                var nameList = game.name.ToList();
-                foreach(var name in nameList)
+                if (numberOfGames >= 99)
                 {
-                    if (name.primary!= null && name.primary == "true")
+                    currentRequest = currentRequest + id;
+
+                    string url = "https://www.boardgamegeek.com/xmlapi/boardgame/" + currentRequest;
+
+                    HttpResponseMessage test = _client.GetAsync(new Uri(url)).Result;
+
+                    EncodingProvider provider = new CustomUtf8EncodingProvider();
+                    Encoding.RegisterProvider(provider);
+
+                    if (test.StatusCode == HttpStatusCode.OK)
                     {
-                        gamelist.name = name.Value;
+                        XDocument xdoc = XDocument.Parse(test.Content.ReadAsStringAsync().Result);
+
+                        StringReader sr = new StringReader(xdoc.ToString());
+
+                        XmlSerializer serializer = new XmlSerializer(typeof(boardgames));
+
+                        boardgames result = (boardgames)serializer.Deserialize(sr);
+
+                        var responsegameslist = result.boardgame.ToList();
+                        foreach (var game in responsegameslist)
+                        {
+
+                            GamesList gamelist = new GamesList();
+                            List<GamesMechanic> mechanicslist = new List<GamesMechanic>();
+                            List<GamesFamily> familieslist = new List<GamesFamily>();
+
+                            gamelist.id = Convert.ToInt64(game.objectid);
+
+                            var nameList = game.name.ToList();
+                            foreach (var name in nameList)
+                            {
+                                if (name.primary != null && name.primary == "true")
+                                {
+                                    gamelist.name = name.Value;
+                                }
+                            }
+
+                            gamelist.minPlayers = Convert.ToInt32(game.minplayers);
+                            gamelist.maxPlayers = Convert.ToInt32(game.maxplayers);
+                            gamelist.thumbnail = game.thumbnail;
+
+                            var mechanicList = game.boardgamemechanic?.ToList();
+                            if (mechanicList != null)
+                            {
+                                foreach (var mechanic in mechanicList)
+                                {
+                                    GamesMechanic gameMechanic = new GamesMechanic();
+                                    gameMechanic.id = Convert.ToInt64(game.objectid);
+                                    gameMechanic.mechanic = mechanic.Value;
+                                    mechanicslist.Add(gameMechanic);
+                                }
+                            }
+
+                            var familyList = game.boardgamefamily?.ToList();
+                            if (familyList != null)
+                            {
+                                foreach (var family in familyList)
+                                {
+                                    GamesFamily gameFamily = new GamesFamily();
+                                    gameFamily.id = Convert.ToInt64(game.objectid);
+                                    gameFamily.family = family.Value;
+                                    familieslist.Add(gameFamily);
+                                }
+                            }
+
+
+                            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("App")))
+                            {
+                                connection.Query<GamesList>($"dbo.[InsertGameinList] @id, @name, @minPlayers, @maxPlayers, @thumbnail",
+                                    new { gamelist.id, gamelist.name, gamelist.minPlayers, gamelist.maxPlayers, gamelist.thumbnail });
+                                foreach (GamesMechanic mechanic in mechanicslist)
+                                {
+                                    connection.Query<GamesMechanic>($"dbo.[InsertGameMechanics] @id, @mechanic",
+                                        new { mechanic.id, mechanic.mechanic });
+                                }
+
+                                foreach (GamesFamily family in familieslist)
+                                {
+                                    connection.Query<GamesFamily>($"dbo.[InsertGameFamilies] @id, @family",
+                                        new { family.id, family.family });
+                                }
+                            }
+
+                        }
+
                     }
-                }
 
-                gamelist.minPlayers = Convert.ToInt32(game.minplayers);
-                gamelist.maxPlayers = Convert.ToInt32(game.maxplayers);
-                gamelist.thumbnail = game.thumbnail;
-
-                var mechanicList = game.boardgamemechanic.ToList();
-                foreach(var mechanic in mechanicList)
+                    currentRequest = "";
+                    numberOfGames = 0;
+                    Thread.Sleep(5000);
+                } else
                 {
-                    GamesMechanic gameMechanic = new GamesMechanic();
-                    gameMechanic.id = Convert.ToInt64(game.objectid);
-                    gameMechanic.mechanic = mechanic.Value;
-                    mechanicslist.Add(gameMechanic);
+                    currentRequest = currentRequest + id + ",";
+                    numberOfGames += 1;
                 }
 
-                var familyList = game.boardgamemechanic.ToList();
-                foreach (var family in familyList)
-                {
-                    GamesFamily gameFamily = new GamesFamily();
-                    gameFamily.id = Convert.ToInt64(game.objectid);
-                    gameFamily.family = family.Value;
-                    familieslist.Add(gameFamily);
-                }
-
-                var ops = "test";
             }
+
+           
 
 
 
