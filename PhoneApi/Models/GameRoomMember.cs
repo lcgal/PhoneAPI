738499c592161eb5a9d95a8b4d138c4c59df7a01
Lucas Galvão <lcollares@indracompany.com>
2020-07-19 19:44:43 -0300

    OfferGameApi tracking untracked files

---
+++ PhoneApi/Models/GameRoomMember.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhoneApi.Models
+{
+    public class GameRoomMember
+    {
+        public Guid OfferId { get; set; }
+
+        public Guid UserId { get; set; }
+
+        public bool IsHost { get; set; }
+    }
+}
