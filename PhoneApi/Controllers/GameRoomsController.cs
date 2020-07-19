e971f45dbc88f4c802e55c3232db3754702b49b7
Lucas Galvão <lcollares@indracompany.com>
2020-07-19 12:34:23 -0300

    OfferGameApi implementing offergame method, and ignoring uncommented variables warnings

--- PhoneApi/Controllers/GameRoomsController.cs
+++ PhoneApi/Controllers/GameRoomsController.cs
@@ -1,8 +1,10 @@
-﻿using PhoneApi.Models;
+﻿using Dapper;
+using PhoneApi.Models;
 using PhoneApi.Models.Responses;
 using PhoneAPI.Utils;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -10,16 +12,37 @@ using System.Web.Http;
 
 namespace PhoneApi.Controllers
 {
+    [RoutePrefix("gamerooms")]
     public class GameRoomsController : ApiController
     {
         [HttpPost]
         [Route("offergame")]
-        public BooleanResponse OfferGame([FromBody]Profile profile)
+        public ApiResponse<Guid> OfferGame([FromBody]GameOffer offer)
         {
+            ApiResponse<Guid> response = new ApiResponse<Guid>();
+            try
+            {
+                Guid id = InsertGameOffer(offer);
+                if (id != null)
+                {
+                    response.ReturnData = id;
+                    response.Result = true;
+                }
 
+            } 
+            catch (Exception e)
+            {
+                response.Error = e.Message;
+            }
+            return response;
 
-            return null;
-
+        }
+        private Guid InsertGameOffer(GameOffer offer)
+        {
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("App")))
+            {
+                return connection.Query<Guid>($"dbo.[CreateGameOffer] @gameid, @latitude, @longitude", new { offer.Gameid, offer.Latitude, offer.Longitude }).FirstOrDefault();
+            }
         }
     }
 }
