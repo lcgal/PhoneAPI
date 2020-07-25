b220b8e2eb3fa5ae5e508116ce7c606432154c53
Lucas Galvão <lcollares@indracompany.com>
2020-07-25 01:01:01 -0300

    OfferGameApi versioning folder

---
+++ PhoneApi/Models/Payloads/UserLocationPayload.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhoneApi.Models.Payloads
+{
+    public class UserLocationPayload
+    {
+        public Guid UserId { get; set; }
+
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
+    }
+}
